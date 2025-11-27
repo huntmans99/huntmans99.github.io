2217431eaa62f90e33c94f3f6a9c7747917e8bf7
McDonald Komla Agbaklah <mcdonaldkomla@gmail.com>
2025-11-27 17:44:24 +0000

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,27 @@
+import type { NextConfig } from "next";
+import path from "node:path";
+
+const LOADER = path.resolve(__dirname, 'src/visual-edits/component-tagger-loader.js');
+
+const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: '',
+  images: {
+    unoptimized: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  turbopack: {
+    rules: {
+      "*.{jsx,tsx}": {
+        loaders: [LOADER]
+      }
+    }
+  }
+};
+
+export default nextConfig;
